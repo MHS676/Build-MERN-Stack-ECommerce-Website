0f09f7b4ec1cd159e528e79cc8c65b1bf4d1afef
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-12-03 17:49:54 +0600

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div >
-    <header>
-      <Link to = "/"> universe </Link>
-    </header>
-    <main>
-    <Routes>
-    <Route path="/product/:slug" element = {<ProductScreen />} />
-    <Route path="/" element = {<HomeScreen/>}/>
-    </Routes>
-    </main>
-    </div>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <div>
+          <header>
+            <Navbar bg="dark" variant="dark">
+              <Container>
+                <LinkContainer to="/">
+                  <Navbar.Brand>universe</Navbar.Brand>
+                </LinkContainer>
+              </Container>
+            </Navbar>
+          </header>
+          <main>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
