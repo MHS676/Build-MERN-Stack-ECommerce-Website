37017f75ab7d54ffeae179eec3f1136406c960b3
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-12-06 22:36:56 +0600

    package changes

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,19 @@ import data from './data.js';
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
+  res.send(data.products);
 });
 
 app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find(x => x.slug === req.params.slug);
-    if(product) {
-        res.status(404).send ({massage: 'Product Not Found'})
-    }
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
